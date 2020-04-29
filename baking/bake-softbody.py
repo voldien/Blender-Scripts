04a62bb57a0ddafcbbb2d0f5cf8825f782da309d
voldie <voldiekami@gmail.com>
2020-04-29 16:22:15 +0200

    Resolved bug with passing wrong argument.

--- baking/bake-softbody.py
+++ baking/bake-softbody.py
@@ -5,8 +5,7 @@ for scene in bpy.data.scenes:
         for modifier in object.modifiers:
             if modifier.type == 'SOFT_BODY':
                 print("Baking Softbody:", object.name)
-                override = {'scene': scene, 'active_object': object,
-                            'point_cache': modifier.domain_settings.point_cache}
+                override = {'scene': scene, 'active_object': object, 'point_cache': modifier.point_cache}
                 bpy.ops.ptcache.free_bake(override)
                 bpy.ops.ptcache.bake(override, bake=True)
 
