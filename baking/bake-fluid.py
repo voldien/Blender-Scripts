4b1eab1d7f58a7bbeb0d9e4b2a245afb44564d3e
voldie <voldiekami@gmail.com>
2020-10-02 13:01:23 +0200

    Rendering scripts.

--- baking/bake-fluid.py
+++ baking/bake-fluid.py
@@ -1,12 +1,22 @@
 import bpy
 
+
+screen = bpy.context.screen
+data = bpy.data
+area = bpy.context.area
+blend_data = bpy.context.blend_data
+screen = bpy.context.screen
+window = bpy.context.window
+object = bpy.context.object
+region = bpy.context.region
+
 for scene in bpy.data.scenes:
     for sobj in scene.objects:
         for modifier in sobj.modifiers:
             if modifier.type == 'FLUID_SIMULATION':
                 if modifier.settings.type == 'DOMAIN':
                     print("Baking Fluid Simulation:", sobj.name)
-                    override = {'scene': scene, 'active_object': sobj}
+                    override = {'scene': scene, 'active_object': sobj, 'screen': screen, 'area' : area, 'blend_data' : blend_data, 'window' : window, 'object':sobj, 'region':region}
                     bpy.ops.fluid.bake(override)
 
 bpy.ops.wm.save_mainfile()
---
+++ gpu-rendering/gpu_utils/__init__.py
@@ -0,0 +1,4 @@
+from check_device import check_and_print_cycles_devices
+
+if __name__ == '__main__':
+	check_and_print_cycles_devices()
